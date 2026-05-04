8dbeac728dba13fc3a45dad30d6b748c57b1bc9a
Lichessbotacc <110300918+Lichessbotacc@users.noreply.github.com>
2026-05-04 02:17:40 +0200

    Update tournament_games_ranking.py

--- tournament_games_ranking.py
+++ tournament_games_ranking.py
@@ -2,7 +2,7 @@ import requests
 import json
 from collections import defaultdict
 
-TOURNEY_ID = "Mu4COtPZ"  # deine Turnier-ID
+TOURNEY_ID = "dhpdDr8L"  # deine Turnier-ID
 
 url = f"https://lichess.org/api/tournament/{TOURNEY_ID}/games"
 
