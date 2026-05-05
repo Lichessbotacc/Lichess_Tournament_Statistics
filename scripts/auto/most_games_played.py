66e769cdd299f4cd0dc93c3f49f2bfd4da21aa5c
Lichessbotacc <110300918+Lichessbotacc@users.noreply.github.com>
2026-05-05 07:43:31 +0200

    Update most_games_played.py

--- scripts/auto/most_games_played.py
+++ scripts/auto/most_games_played.py
@@ -3,7 +3,7 @@ import json
 from collections import defaultdict
 
 TEAM_ID = "darkonteams"
-MAX_TOURNEYS = 1000
+MAX_TOURNEYS = 100
 TOURNEY_KEYWORD = "Hourly Ultrabullet"
 
 ONLY_TEAM_MEMBERS = False   # True = nur Teamspieler
