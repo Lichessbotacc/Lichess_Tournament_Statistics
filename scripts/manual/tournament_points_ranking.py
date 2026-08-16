773713888b147bf5bc867ba03d43d2602b50406a
Lichessbotacc <110300918+Lichessbotacc@users.noreply.github.com>
2026-08-16 16:08:10 +0200

    Update tournament IDs in ranking script

--- scripts/manual/tournament_points_ranking.py
+++ scripts/manual/tournament_points_ranking.py
@@ -5,7 +5,7 @@ from collections import defaultdict
 
 # 🔥 SETTINGS
 TARGET_TEAM = "darkonteams"
-TOURNEY_IDS = ["re9uGBoG", "7sXReC3X", "40PE8QwA", "Ch1QoC9R", "8WV6sLeF", "3x3EqQe4", "NlKORTVL", "P2O2ivkV", "CDz2PEg2"]
+TOURNEY_IDS = ["WxgaONHC", "HQxblt5W", "woTLfiqv", "Ch1QoC9R", "NHp5rNCJ",]
 REQUEST_DELAY = 0  # Sekunden Pause zwischen Requests (Rate-Limit-Schutz)
 MAX_RETRIES = 3
 
